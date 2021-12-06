08ad98343b159f2292065ca31f7c8c0c530dbbe6
Edwin Chiu <inceptionev@gmail.com>
2021-12-05 20:46:34 -0800

    working demo.  improved fonts, but screen flickers

--- RotaryFilmProcessor.ino
+++ RotaryFilmProcessor.ino
@@ -1,5 +1,6 @@
 #include <M5Core2.h>
 #include <Tic.h>
+#include "Free_Fonts.h"
 
 TicI2C tic;
 
@@ -9,9 +10,12 @@ int startTime = 0;
 int nowTime = 0;
 int displayMin = 0;
 char displaySec[10];
+char displayBuffer[10];
 
 #define BGCOLOR BLACK
 #define TEXTCOLOR WHITE
+#define TITLE_FONT FMB12
+#define TIMER_FONT FMB24
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,9 +23,8 @@ void setup() {
   M5.begin(true, false, true, true);  //Init M5Core, including i2c begin.  LCD on, SD card off, Serial on, I2C on
 
   M5.Lcd.setTextColor(TEXTCOLOR, BGCOLOR);
-  M5.Lcd.setTextSize(2);
-  M5.Lcd.setCursor(65, 10);
-  M5.Lcd.print("Rotary Processor");
+  M5.Lcd.setFreeFont(TITLE_FONT);
+  M5.Lcd.drawString("Rotary Processor", 45, 10, GFXFF);
 
   delay(20);
   tic.exitSafeStart();
@@ -47,12 +50,12 @@ void loop() {
 
   if (runMotor) {
     nowTime = millis();
-    displayMin = floor((nowTime-startTime)/60000);
-    sprintf(displaySec,"%04.1f",((nowTime-startTime)%60000)/(float)1000);
-    M5.Lcd.setCursor(65, 30);
-    M5.Lcd.print(displayMin);
-    M5.Lcd.print(":");
-    M5.Lcd.print(displaySec);
+    M5.Lcd.setFreeFont(TIMER_FONT);
+    sprintf(displayBuffer,"%d:%04.1f ",floor((nowTime-startTime)/60000),((nowTime-startTime)%60000)/(float)1000);
+    M5.Lcd.drawString(displayBuffer, 70, 70, GFXFF);
+
   }
+
+  delay(30);
 
 }
